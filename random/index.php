fdfe53684fc5bcd25b2d8b20cfe7081a3ca6c463
mino-bit <colonna.davide.01@gmail.com>
2025-11-24 17:06:27 +0100

    push dei div inseriti tramite un for

--- random/index.php
+++ random/index.php
@@ -7,6 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>css casuali</h1>
+    <?php
+        for($i=0;$i<15;$i++){
+
+            if($i % 2 == 0){
+                echo "<div class=pari> Questo è il div numero ". $i+1 . "</div>";
+            
+            }else{
+                echo "<div class=dispari> Questo è il div numero ". $i+1 . "</div>";
+                }
+        }
+    ?>
+    <div> questa è la fine della pagina</div>
 </body>
-</html>m
+</html>
