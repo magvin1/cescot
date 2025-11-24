e66f2cd0084a7d0a96a2a15d9610601a3f3c3e84
magvin1 <magliovi@hotmail.it>
2025-11-24 16:17:20 +0100

    php fatto e unito a page.php

--- php/page.php
+++ php/page.php
@@ -1,3 +1,17 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>pagina di prova</h1>
+    <?php
+        for($i=0;$i<15;$i++){
+            echo "<div> Questo è il div numero ". $i+1;
+            echo " </div>";
+        }
+    ?>
+</body>
+</html>
