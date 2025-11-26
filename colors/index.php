fc5b095fb42854d0e98f3250e9f6d0928ccf7a15
magvin1 <magliovi@hotmail.it>
2025-11-26 16:52:46 +0100

    fatta generazione div

--- colors/index.php
+++ colors/index.php
@@ -8,6 +8,18 @@
 </head>
 <body>
     <h1>colori</h1>
+    <?php
+        $random=rand(5,30);
+        genera_div($random);
+    ?>
     <div id="divFinale">div finale</div>
 </body>
 </html>
+
+<?php
+    function genera_div($numero_div){
+        for($i=0; $i<$numero_div; $i++){
+            echo "<div class='div_generato'>div generato</div>";
+        }
+    }
+?>
