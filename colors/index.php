c47eaa16aab73875b450d53640763a9b6dba93f3
magvin1 <magliovi@hotmail.it>
2025-11-26 17:04:31 +0100

    div generati migliorati grazie a una funzione innovativa ceh ha cambiato il mondo

--- colors/index.php
+++ colors/index.php
@@ -10,16 +10,16 @@
     <h1>colori</h1>
     <?php
         $random=rand(5,30);
-        genera_div($random);
+        genera_div($random, "div generato");
     ?>
     <div id="divFinale">div finale</div>
 </body>
 </html>
 
 <?php
-    function genera_div($numero_div){
+    function genera_div($numero_div, $testodiv){
         for($i=0; $i<$numero_div; $i++){
-            echo "<div class='div_generato'>div generato</div>";
+            echo "<div class='div_generato'>". $testodiv. "</div>";
         }
     }
 ?>
