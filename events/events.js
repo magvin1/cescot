23f58a556c6164f70112542ecc154f316ea26a6b
magvin1 <magliovi@hotmail.it>
2025-12-03 17:11:39 +0100

    Fix event listener to correctly change background color of divs

--- events/events.js
+++ events/events.js
@@ -12,7 +12,7 @@ function sfondoDivRosso(){
     mioBtn.addEventListener("click",function(){
         let divs =document.getElementsByClassName('cambia_colore');
         for(let i=0; i< mioDiv.length; i++){
-        mioDiv[i].style.backgroundColor = 'red';
+        divs[i].style.backgroundColor = 'red';
         }
     })
 }
