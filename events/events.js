58e114bbf8ee19e7ae8f6d1c8a41614d24aef4ad
magvin1 <magliovi@hotmail.it>
2025-12-03 16:33:15 +0100

    Refactor sfondoDivRosso function for improved readability

--- events/events.js
+++ events/events.js
@@ -1,7 +1,6 @@
-/*mo ci scriviamo qualcosa*/
 function sfondoDivRosso(){
-let mioDiv = document.getElementsByClassName('cambia_colore');
-for(let i=0; i< mioDiv.length; i++){
-mioDiv[i].style.backgroundColor = 'red';
-}
+    let mioDiv = document.getElementsByClassName('cambia_colore');
+    for(let i=0; i< mioDiv.length; i++){
+    mioDiv[i].style.backgroundColor = 'red';
+    }
 }
