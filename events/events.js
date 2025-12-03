ca4465a1cd7c186cc63ad623a529168519682df6
magvin1 <magliovi@hotmail.it>
2025-12-03 17:11:03 +0100

    Refactor sfondoDivRosso function to include commented-out button event listener

--- events/events.js
+++ events/events.js
@@ -5,3 +5,15 @@ function sfondoDivRosso(){
     }
 }
 //si
+/*
+function sfondoDivRosso(){
+    let mioBtn=document.getElementById('btn_colora');
+
+    mioBtn.addEventListener("click",function(){
+        let divs =document.getElementsByClassName('cambia_colore');
+        for(let i=0; i< mioDiv.length; i++){
+        mioDiv[i].style.backgroundColor = 'red';
+        }
+    })
+}
+*/
