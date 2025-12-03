437d1d8ee2bb87d68c89f6398217f95e13006661
magvin1 <magliovi@hotmail.it>
2025-12-03 16:05:25 +0100

    Add onclick event to color button for background change

--- events/index.php
+++ events/index.php
@@ -4,10 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Events</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <h1> Eventi javascript</h1>
-        <button id="btn_colora">Colora</button>
+        <button id="btn_colora" onclick="sfondoDivRosso()">Colora</button>
         <?php
             for($i=0;$i<20;$i++){
                 echo "<div> Div numero ". $i+1 . "</div>";
