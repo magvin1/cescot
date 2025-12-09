243a7095d3061466cf7e7184952a58bd57db9906
mino-bit <colonna.davide.01@gmail.com>
2025-12-09 19:44:25 +0100

    risoluzione problemi

--- events/events.js
+++ events/events.js
@@ -1,19 +1,19 @@
-let divs = document.getElementsByClassName('cambia_colore');
 function sfondoDivRosso(){
-    
+    let divs = document.getElementsByClassName('cambia_colore');
     for(let i = 0; i < divs.length; i++){
         divs[i].style.backgroundColor = 'red';
     }
 }
 
 function sfondoDivGiallo(){
-   
+    let divs = document.getElementsByClassName('cambia_colore');
     for(let i = 0; i < divs.length; i++){
         divs[i].style.backgroundColor = 'yellow';
     }
 }
 
 function sfondoDivBlu(){
+    let divs = document.getElementsByClassName('cambia_colore');
     for(let i = 0; i < divs.length; i++){
         divs[i].style.backgroundColor = 'blue';
     }
