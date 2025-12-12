d2f27da9eb5d7360d22b1b85ddb7da1c6219fa65
Haelusus <graziadevivo13@gmail.com>
2025-12-12 12:34:46 +0100

    Refactor index.php by removing unused functions and streamlining div generation logic

--- colors/index.php
+++ colors/index.php
@@ -1,3 +1,6 @@
+<?php
+    include '../cescot/libreria.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,47 +13,9 @@
     <h1>colori</h1>
     <?php
     //dichiaro variabile random
-        $random=rand(5,30);
-        genera_div($random, "div generato");
+        genera_div(rand(5,30), "div generato");
+        genera_colore_casuale();
     ?>
     <div id="divFinale">div finale</div>
 </body>
 </html>
-
-<?php
-    function genera_div($numero_div, $testodiv){
-        for($i=0; $i<$numero_div; $i++){
-            $colore=genera_colore_casuale();
-
-            switch($colore){
-                case "red":
-                    echo "<div class='divRosso'>". $testodiv. "</div>";
-                    break;
-                case "green":
-                    echo "<div class='divVerde'>". $testodiv. "</div>";
-                    break;
-                case "blue":
-                    echo "<div class='divBlu'>". $testodiv. "</div>";
-                    break;
-                case "yellow":
-                    echo "<div class='divGiallo'>". $testodiv. "</div>";
-                    break;
-            }
-
-        }
-    }
-
-    function genera_colore_casuale(){
-    $random=rand(1,4);
-        switch($random){
-            case 1:
-                return "red";
-            case 2:
-                return "green";
-            case 3:
-                return "blue";
-            case 4:
-                return "yellow";
-        }
-}
-?>
