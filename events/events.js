14f478c757bdbdfc5161e87278e6f63c3978c222
mino-bit <colonna.davide.01@gmail.com>
2025-12-12 12:42:38 +0100

    refactoring secondo gruppo di 4 cartelle

--- events/events.js
+++ events/events.js
@@ -20,36 +20,48 @@ function sfondoDivBlu(){
 }
 */
 function cambiaColoreDiv(coloreSfondo){
-let divs = document.getElementsByClassName('cambia_colore');
-switch(coloreSfondo){
-case 'red':
-for(let i = 0; i < divs.length; i++){
-        divs[i].style.backgroundColor = 'red';
+    // seleziona tutti gli elementi con la classe 'cambia_colore'
+    let divs = document.getElementsByClassName('cambia_colore');
+    // scegli il colore in base al parametro passato
+    switch(coloreSfondo){
+        case 'red':
+            for(let i = 0; i < divs.length; i++){
+                // imposta lo sfondo di ogni div su rosso
+                divs[i].style.backgroundColor = 'red';
+            }
+            break;
+        case 'yellow':
+            for(let i = 0; i < divs.length; i++){
+                // imposta lo sfondo di ogni div su giallo
+                divs[i].style.backgroundColor = 'yellow';
+            }
+            break;
+        case 'blue':
+            for(let i = 0; i < divs.length; i++){
+                // imposta lo sfondo di ogni div su blu
+                divs[i].style.backgroundColor = 'blue';
+            }
+            break;
     }
-break;
-case 'yellow':
-for(let i = 0; i < divs.length; i++){
-        divs[i].style.backgroundColor = 'yellow';
-    }
-break;
-case 'blue':
-for(let i = 0; i < divs.length; i++){
-        divs[i].style.backgroundColor = 'blue';
-    }
-break;
-}
 
 }
 
 function alternaSfondoDiv(){
+    // seleziona tutti gli elementi con la classe 'cambia_colore'
     let divs = document.getElementsByClassName('cambia_colore');
+    // ciclo con passo 3: imposta i div in posizione 0,3,6,... su rosso
     for(let i = 0; i < divs.length; i+= 3){
+        // posizione modulo 3 == 0 -> rosso
         divs[i].style.backgroundColor = 'red';
     }
+    // ciclo con passo 3: imposta i div in posizione 1,4,7,... su giallo
     for(let i = 1; i < divs.length; i+= 3){
+        // posizione modulo 3 == 1 -> giallo
         divs[i].style.backgroundColor = 'yellow';
     }
+    // ciclo con passo 3: imposta i div in posizione 2,5,8,... su blu
     for(let i = 2; i < divs.length; i+= 3){
+        // posizione modulo 3 == 2 -> blu
         divs[i].style.backgroundColor = 'blue';
     }
 }
--- lib/libreria.php
+++ lib/libreria.php
@@ -36,4 +36,40 @@
                 return "yellow";
         }
     }
+
+    
+    function cambiaColoreDiv(coloreSfondo){
+    let divs = document.getElementsByClassName('cambia_colore');
+    switch(coloreSfondo){
+    case 'red':
+    for(let i = 0; i < divs.length; i++){
+            divs[i].style.backgroundColor = 'red';
+        }
+    break;
+    case 'yellow':
+    for(let i = 0; i < divs.length; i++){
+            divs[i].style.backgroundColor = 'yellow';
+        }
+    break;
+    case 'blue':
+    for(let i = 0; i < divs.length; i++){
+            divs[i].style.backgroundColor = 'blue';
+        }
+    break;
+    }
+
+    }
+
+    function alternaSfondoDiv(){
+        let divs = document.getElementsByClassName('cambia_colore');
+        for(let i = 0; i < divs.length; i+= 3){
+            divs[i].style.backgroundColor = 'red';
+        }
+        for(let i = 1; i < divs.length; i+= 3){
+            divs[i].style.backgroundColor = 'yellow';
+        }
+        for(let i = 2; i < divs.length; i+= 3){
+            divs[i].style.backgroundColor = 'blue';
+        }
+    }
 ?>
