da02f506ebf705853633d3219d7662883a58c4e0
mino-bit <colonna.davide.01@gmail.com>
2025-12-15 12:35:09 +0100

    Refactor cambiaColoreDiv to apply color changes to all elements in the specified groups

--- groups/groups.js
+++ groups/groups.js
@@ -1,34 +1,58 @@
 //eventuale js
 function cambiaColoreDiv(coloreSfondo){
     // seleziona tutti gli elementi con la classe 'cambia_colore'
-    let divs = document.getElementsByClassName('primo_gruppo');
+    let divs;
     // scegli il colore in base al parametro passato
     switch(coloreSfondo){
         case 'first_red':
+            divs = document.getElementsByClassName('primo_gruppo');
+            for(let i = 0; i < divs.length; i++){
                 // imposta lo sfondo del primo div su rosso
-                divs[0].style.backgroundColor = 'red';
+                divs[i].style.backgroundColor = 'red';
+            }
             break;
         case 'second_red':
-                // imposta lo sfondo del secondo div su rosso
-                divs[1].style.backgroundColor = 'red';
+            divs = document.getElementsByClassName('secondo_gruppo');
+            for(let i = 0; i < divs.length; i++){
+                // imposta lo sfondo del primo div su rosso
+                divs[i].style.backgroundColor = 'red';
+            }
             break;
         case 'every_red':
+             divs = document.getElementsByClassName('primo_gruppo');
             for(let i = 0; i < divs.length; i++){
-                // imposta lo sfondo di ogni div su rosso
+                // imposta lo sfondo di ogni div su blu
+                divs[i].style.backgroundColor = 'red';
+            }
+            divs = document.getElementsByClassName('secondo_gruppo');
+            for(let i = 0; i < divs.length; i++){
+                // imposta lo sfondo del primo div su rosso
                 divs[i].style.backgroundColor = 'red';
             }
             break;
         case 'first_blue':
-                // imposta lo sfondo del primo div su blu
-                divs[0].style.backgroundColor = 'blue';
+            divs = document.getElementsByClassName('primo_gruppo');
+            for(let i = 0; i < divs.length; i++){
+                // imposta lo sfondo del primo div su rosso
+                divs[i].style.backgroundColor = 'blue';
+            }
             break;
         case 'second_blue':
-                // imposta lo sfondo del secondo div su blu
-                divs[1].style.backgroundColor = 'blue';
+            divs = document.getElementsByClassName('secondo_gruppo');
+            for(let i = 0; i < divs.length; i++){
+                // imposta lo sfondo del primo div su rosso
+                divs[i].style.backgroundColor = 'blue';
+            }
             break;
         case 'every_blue':
+             divs = document.getElementsByClassName('primo_gruppo');
             for(let i = 0; i < divs.length; i++){
                 // imposta lo sfondo di ogni div su blu
+                divs[i].style.backgroundColor = 'blue';
+            }
+            divs = document.getElementsByClassName('secondo_gruppo');
+            for(let i = 0; i < divs.length; i++){
+                // imposta lo sfondo del primo div su rosso
                 divs[i].style.backgroundColor = 'blue';
             }
             break;
