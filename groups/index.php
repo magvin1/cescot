13122b015afa182978749277ee4772d8702942d6
mino-bit <colonna.davide.01@gmail.com>
2025-12-15 12:17:29 +0100

    Fix button onclick parameters for color change functionality

--- groups/index.php
+++ groups/index.php
@@ -11,12 +11,12 @@
     </head>
     <body>
         <div>
-        <button id="first_red" onclick="cambiaColoreDiv('red')">Primo Rosso</button>
-        <button id="second_red" onclick="cambiaColoreDiv('red')">Secondo Rosso</button>
-        <button id="every_red" onclick="cambiaColoreDiv('red')">Tutti Rossi</button>
-        <button id="first_blue" onclick="cambiaColoreDiv('blue')">Primo Blu</button>
-        <button id="second_blue" onclick="cambiaColoreDiv('blue')">Secondo Blu</button>
-        <button id="every_blue" onclick="alternaSfondoDiv('blue')">Tutti Blu</button>
+        <button id="first_red" onclick="cambiaColoreDiv('first_red')">Primo Rosso</button>
+        <button id="second_red" onclick="cambiaColoreDiv('second_red')">Secondo Rosso</button>
+        <button id="every_red" onclick="cambiaColoreDiv('every_red')">Tutti Rossi</button>
+        <button id="first_blue" onclick="cambiaColoreDiv('first_blue')">Primo Blu</button>
+        <button id="second_blue" onclick="cambiaColoreDiv('second_blue')">Secondo Blu</button>
+        <button id="every_blue" onclick="alternaSfondoDiv('every_blue')">Tutti Blu</button>
 
         <h2 class='first_group'><?php crea_div(rand(5,10), 'primo_gruppo')?></h2>
         <h2 class='second_group'><?php crea_div(rand(10,15), 'secondo_gruppo')?></h2>
