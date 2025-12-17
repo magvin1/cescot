177e3839567ed1a0dd9ab61ccc49f51cf08bcbca
magvin1 <magliovi@hotmail.it>
2025-12-17 12:16:51 +0100

    aggiustati gli every di groups

--- groups/groups.js
+++ groups/groups.js
@@ -20,7 +20,13 @@ function cambiaColoreDiv(coloreSfondo){
                 divs[i].style.backgroundColor = 'red';
             break;
         case 'every_red': //imposta lo sfondo di ogni gruppo su rosso
-             divs = document.getElementsByClassName('primo_gruppo','secondo_gruppo','terzo_gruppo');
+            divs = document.getElementsByClassName('primo_gruppo');
+            for(let i = 0; i < divs.length; i++)
+                divs[i].style.backgroundColor = 'red';
+            divs = document.getElementsByClassName('secondo_gruppo');
+            for(let i = 0; i < divs.length; i++)
+                divs[i].style.backgroundColor = 'red';
+            divs = document.getElementsByClassName('terzo_gruppo');
             for(let i = 0; i < divs.length; i++)
                 divs[i].style.backgroundColor = 'red';
             break;
@@ -34,36 +40,48 @@ function cambiaColoreDiv(coloreSfondo){
             for(let i = 0; i < divs.length; i++)
                 divs[i].style.backgroundColor = 'blue';
             break;
-            case 'third_blue': // imposta lo sfondo del secondo gruppo div su blu
+        case 'third_blue': // imposta lo sfondo del secondo gruppo div su blu
+                divs = document.getElementsByClassName('terzo_gruppo');
+                for(let i = 0; i < divs.length; i++)
+                    divs[i].style.backgroundColor = 'blue';
+            break;
+        case 'every_blue': // imposta lo sfondo di ogni div su blu
+            divs = document.getElementsByClassName('primo_gruppo');
+            for(let i = 0; i < divs.length; i++)
+                divs[i].style.backgroundColor = 'blue';
+            divs = document.getElementsByClassName('secondo_gruppo');
+            for(let i = 0; i < divs.length; i++)
+                divs[i].style.backgroundColor = 'blue';
             divs = document.getElementsByClassName('terzo_gruppo');
             for(let i = 0; i < divs.length; i++)
                 divs[i].style.backgroundColor = 'blue';
             break;
-        case 'every_blue': // imposta lo sfondo di ogni div su blu
-             divs = document.getElementsByClassName('primo_gruppo','secondo_gruppo','terzo_gruppo');
-            for(let i = 0; i < divs.length; i++)
-                divs[i].style.backgroundColor = 'blue';
-            break;
-            case 'first_yellow': // imposta lo sfondo del primo gruppo div su rosso
+        case 'first_yellow': // imposta lo sfondo del primo gruppo div su rosso
             divs = document.getElementsByClassName('primo_gruppo');
             for(let i = 0; i < divs.length; i++)
                 divs[i].style.backgroundColor = 'yellow';
             break;
-            case 'second_yellow': // imposta lo sfondo del secondo gruppo div su blu
+        case 'second_yellow': // imposta lo sfondo del secondo gruppo div su blu
             divs = document.getElementsByClassName('secondo_gruppo');
             for(let i = 0; i < divs.length; i++)
                 divs[i].style.backgroundColor = 'yellow';
             break;
-            case 'third_yellow': // imposta lo sfondo del secondo gruppo div su blu
+        case 'third_yellow': // imposta lo sfondo del secondo gruppo div su blu
             divs = document.getElementsByClassName('terzo_gruppo');
             for(let i = 0; i < divs.length; i++)
                 divs[i].style.backgroundColor = 'yellow';
             break;
-            case 'every_yellow':
-                divs = document.getElementsByClassName('primo_gruppo','secondo_gruppo','terzo_gruppo');
+        case 'every_yellow':
+            divs =document.getElementsByClassName('primo_gruppo');
             for(let i = 0; i < divs.length; i++)
                 divs[i].style.backgroundColor = 'yellow';
-                break;
+            divs =document.getElementsByClassName('secondo_gruppo');
+            for(let i = 0; i < divs.length; i++)
+                divs[i].style.backgroundColor = 'yellow';
+            divs =document.getElementsByClassName('terzo_gruppo');
+            for(let i = 0; i < divs.length; i++)
+                divs[i].style.backgroundColor = 'yellow';
+            break;
         default: // In caso di errori imposta lo sfondo di ogni div su verde
             for(let i = 0; i < divs.length; i++)
                 divs[i].style.backgroundColor = 'green';
