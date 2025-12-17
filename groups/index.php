067bec7001d27dbfe31788911f597b5de6a608bf
Haelusus <graziadevivo13@gmail.com>
2025-12-17 11:49:52 +0100

    pusho l'aggiunta del terzo gruppo e dei nuovi div

--- groups/index.php
+++ groups/index.php
@@ -18,8 +18,16 @@
         <button id="second_blue" onclick="cambiaColoreDiv('second_blue')">Secondo Blu</button>
         <button id="every_blue" onclick="cambiaColoreDiv('every_blue')">Tutti Blu</button>
 
+        <button id="third_red" onclick="cambiaColoreDiv('third_red')">Terzo Rosso</button>
+        <button id="third_blue" onclick="cambiaColoreDiv('third_blue')">Terzo Blu</button>
+        <button id="first_yellow" onclick="cambiaColoreDiv('first_yellow')">Primo Giallo</button>
+        <button id="second_yellow" onclick="cambiaColoreDiv('second_yellow')">Secondo Giallo</button>
+        <button id="third_yellow" onclick="cambiaColoreDiv('third_yellow')">Terzo Giallo</button>
+        <button id="every_yellow" onclick="cambiaColoreDiv('every_yellow')">Tutti Giallo</button>
+
         <h2 class='first_group'><?php crea_div(rand(5,10), 'primo_gruppo')?></h2>
         <h2 class='second_group'><?php crea_div(rand(10,15), 'secondo_gruppo')?></h2>
+        <h2 class='third_group'><?php crea_div(rand(5,20), 'terzo_gruppo')?></h2>
         </div>
     </body>
     <script src="groups.js"></script>
