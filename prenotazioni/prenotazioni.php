3c283a681e3ee306834b623472085539b333a160
Mino-bit <colonna.davide.01@gmail.com>
2026-01-13 12:24:13 +0100

    forse qualcosa di buono

--- prenotazioni/prenotazioni.php
+++ prenotazioni/prenotazioni.php
@@ -11,6 +11,26 @@
 </head>
 <body>
     <h1>Prenotazioni</h1>
-    
+    <?php
+    $mysqli= connetti_db("prenotazioni");
+   
+$queryDati= 
+"select c.nome, c.cognome, cit.citta, p.importo,p.arrivo, p.caparra, p.importo- p.caparra as saldo_da_pagare
+from clienti as c
+inner join citta as cit on c.id_citta = cit.citta
+inner join prenotazioni as p on c.id_citta = p.id_citta";
+
+    $result = $mysqli->query($queryDati);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "<div><h2>". $row["p.arrivo"]. "</h2><p>" . $row["c.nome"]. " " . $row["c.cognome"] ." ". $row["cit.citta"]. " " . $row["p.importo"]. " " . $row["p.caparra"]." <span class='saldo'>". $row["p.saldo_da_pagare"]. " " ."</span></p></div>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $mysqli->close();
+    ?>
 </body>
 </html>
