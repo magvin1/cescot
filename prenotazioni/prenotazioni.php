dbee06ba0f9c2af65f21a40c8ee1eaf73a88cc9d
magvin1 <magliovi@hotmail.it>
2026-01-13 12:33:35 +0100

    fix: correct SQL query and variable references in prenotazioni.php

--- prenotazioni/prenotazioni.php
+++ prenotazioni/prenotazioni.php
@@ -13,19 +13,18 @@
     <h1>Prenotazioni</h1>
     <?php
     $mysqli= connetti_db("prenotazioni");
-   
 $queryDati= 
 "select c.nome, c.cognome, cit.citta, p.importo,p.arrivo, p.caparra, p.importo- p.caparra as saldo_da_pagare
 from clienti as c
-inner join citta as cit on c.id_citta = cit.citta
-inner join prenotazioni as p on c.id_citta = p.id_citta";
+inner join citta as cit on c.citta = cit.id_citta
+inner join prenotazioni as p on c.id_cliente = p.cliente";
 
     $result = $mysqli->query($queryDati);
 
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = mysqli_fetch_assoc($result)) {
-                echo "<div><h2>". $row["p.arrivo"]. "</h2><p>" . $row["c.nome"]. " " . $row["c.cognome"] ." ". $row["cit.citta"]. " " . $row["p.importo"]. " " . $row["p.caparra"]." <span class='saldo'>". $row["p.saldo_da_pagare"]. " " ."</span></p></div>";
+                echo "<div><h2>". $row["arrivo"]. "</h2><p>" . $row["nome"]. " " . $row["cognome"] ." ". $row["citta"]. " " . $row["importo"]. " " . $row["caparra"]." <span class='saldo'>". $row["saldo_da_pagare"]. " " ."</span></p></div>";
             }
         } else {
             echo "0 results";
