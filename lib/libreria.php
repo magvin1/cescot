37f5deeef67dd43875dfaf8b9027647277caf62b
Haelusus <graziadevivo13@gmail.com>
2026-01-16 12:31:48 +0100

    aggiunto funzione clienti

--- lib/libreria.php
+++ lib/libreria.php
@@ -117,4 +117,18 @@
             }
             return;
         }
+        
+        function stampa_ricerca_clienti($result){
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<div><h2>". $row["nome"]. " " . $row["cognome"] ."</h2><p>"  
+                    . $row["regione"]. "</p><p>" . $row["area_geografica"]. "</p><p>" 
+                    . $row["citta"]."</p></div>";
+                }
+            } else {
+                echo "0 results";
+            }
+            return;
+        }
 ?>
