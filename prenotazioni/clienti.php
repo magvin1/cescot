778ec17e083cf07f93e2190dbd727f5468ca368e
mino-bit <colonna.davide.01@gmail.com>
2026-01-16 12:32:23 +0100

    refactor: update query to filter distinct regions and modify client search form

--- prenotazioni/clienti.php
+++ prenotazioni/clienti.php
@@ -22,24 +22,32 @@ inner join regioni as r on cit.regione = r.ID_regione";
 Possibile farlo ma devo aggiungere un riconoscitore come per esempio 'as nome_completo' così lo salvo all'interno 
 di tale variabile e posso poi utilizzarlo come variabile all'interno di row, cancellando row nome e row cognome e inserendo unicamente row nome_completo 
 */
-"select c.nome, c.cognome, cit.citta, r.regione, r.area_geografica
-from clienti as c
-inner join citta as cit on c.citta = cit.id_citta
-inner join regioni as r on cit.regione = r.ID_regione";
+"select DISTINCT regione
+from regioni";
 
     $result = $mysqli->query($queryDati);
+    ?>
+ <form method="GET" action="">
+    
+        <label for="cercaCliente">Cerca Cliente:</label>
+        
+    
+    <?php
+echo'<select name=regioni>';
 
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = mysqli_fetch_assoc($result)) {
-                echo "<div><h2>". $row["nome"]. " " . $row["cognome"] ."</h2><p>"  
-                . $row["regione"]. "</p><p>" . $row["area_geografica"]. "</p><p>" 
-                . $row["citta"]."</p></div>";
+                echo "<option value=". $row["regione"]. ">". $row["regione"]."</option>";  
             }
+            echo"</select>";
         } else {
             echo "0 results";
         }
         $mysqli->close();
     ?>
+
+<button type="submit">Filtra</button>
+    </form>
 </body>
 </html>
