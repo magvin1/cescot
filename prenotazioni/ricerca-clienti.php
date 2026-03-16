63ca2a1bb684afe1f70282ec6a47f04934704b2b
Haelusus <graziadevivo13@gmail.com>
2026-03-16 12:48:48 +0100

    some random things

--- prenotazioni/ricerca-clienti.php
+++ prenotazioni/ricerca-clienti.php
@@ -8,9 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <label for="nomeCliente">Cliente:</label>
-    <input type="text" id="nomeCliente" name="nomeCliente">
+<body>   
     <?php
     $mysqli= connetti_db("prenotazioni");
     $queryDati= 
@@ -20,7 +18,8 @@
     $result = $mysqli->query($queryDati);
     ?>
         <form method="GET" action="">
-        <label for="regione">Cerca Cliente:</label>
+        <label for="regione">Cerca regione:</label>
+        
     <?php
         echo'<select name=regione id=regione>';
         if ($result->num_rows > 0) {
@@ -32,16 +31,26 @@
         } else {
             echo "0 results";
         }
+    ?>
+        <label for="nomeCliente">Cliente:</label>
+        <input type="text" id="nomeCliente" name="nomeCliente">
+    <?php
         echo "<button type='submit'>Cerca</button>";
         echo "</form>";
-        
         if (!isset($_GET['regione']) || $_GET['regione'] == "") {
             $check_regione=0;
         } else{
             $regioneScrittaDaUtente= "%".$_GET['regione']."%";
+            $check_regione=1;
         }
-        if(!isset($GET['nome'])){
+
+        if(!isset($_GET['nomeCliente'])){
+            var_dump($_GET['nomeCliente']);
             $check_nome=0;
+        }
+        else{
+            $nome_inserito= "%".$_GET['nomeCliente']; 
+            $check_nome=1;
         }
         if(!$check_nome && !$check_regione){
             $queryDati= 
@@ -56,11 +65,12 @@
         }
         else if(!$check_nome) echo "Non hai inserito nessun nome/cognome da cercare";
         else if(!$check_regione) echo "Non hai inserito nessuna regione da cercare";
-            $result = $mysqli->query($queryDati);
+            $result1 = $mysqli->query($queryDati); 
+            var_dump($result1);
         if ($result->num_rows > 0) {
             // output data of each row
-            while($row = mysqli_fetch_assoc($result)) {
-                echo "<div><h2>". $row[""]. "</h2><p>" . $row["nome"]. " " 
+            while($row = mysqli_fetch_assoc($result1)) { var_dump($row);
+                echo "<div><p>" . $row["nome"]. " " 
                 . $row["cognome"] ." ". $row["citta"]. " " . $row["regione"]. " " 
                 . $row["dataNascita"]."</p></div>";
             }
